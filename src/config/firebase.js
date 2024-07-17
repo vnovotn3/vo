c212c55fe260c6695092d493606f72524ee38db6
Vojtěch Novotný <vojtech@Vojtechs-MacBook-Air.local>
2024-07-17 18:21:42 +0200

    refactor secrets

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -9,7 +9,20 @@ const {
   sendPasswordResetEmail,
 } = require("firebase/auth");
 
-const serviceAccount = require("../../firebaseService.json");
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
+  client_email: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_ADMIN_CLIENT_ID,
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url:
+    process.env.FIREBASE_ADMIN_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_ADMIN_CLIENT_X509_CERT_URL,
+  universe_domain: "googleapis.com",
+};
 
 if (admin.apps.length === 0)
   admin.initializeApp({
